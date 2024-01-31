18cb730453a2229a7624a48aaf3efef3db7b5d6b
Your Name <portia894@gmail.com>
2024-01-31 09:06:43 -0500

    updated index and pseudocode

---
+++ public/script.js
@@ -0,0 +1,7 @@
+//Create API Routes to get all pets, get all pets by name, get pet's by owners name
+//Import Express.js into server.js file
+//Initialize an application: 'app'
+//Create a constant that passes the express file through as required
+//Declare a PORT from our localhost to listen on
+//Use the 'app' object and 'listen method passing in the 'PORT' as an argument
+//Launch server from the command line with node server
